f5e74af944120f982e96046ebb70ef9956e4be4b
TDbouk <toufic.dbouk@gmail.com>
2017-06-01 00:20:05 +0300

    feat: Create RecipeFragment

--- app/src/main/java/tdbouk/udacity/bakingapp/ui/RecipeFragment.java
+++ app/src/main/java/tdbouk/udacity/bakingapp/ui/RecipeFragment.java
@@ -4,60 +4,90 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
 
 import tdbouk.udacity.bakingapp.R;
+import tdbouk.udacity.bakingapp.data.Recipe;
+import tdbouk.udacity.bakingapp.utils.Constants;
+import tdbouk.udacity.bakingapp.utils.Utility;
 
 public class RecipeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private final String TAG = getClass().getSimpleName();
     private OnFragmentInteractionListener mListener;
+    private ArrayList<Recipe> mReceipes = new ArrayList<>();
+    private RecyclerView mRecyclerView;
+    private GridLayoutManager mGridManager;
+    private RecipesAdapter mAdapter;
+
 
     public RecipeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecipeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecipeFragment newInstance(String param1, String param2) {
-        RecipeFragment fragment = new RecipeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+
+        // Request a string response from the provided URL.
+        JsonArrayRequest jsonRequest = new JsonArrayRequest(
+                Request.Method.GET, Constants.JSON_URL, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+//                        Log.d(TAG, "json res: " + response);
+                        mReceipes = Utility.parseRecipeJson(response);
+
+                        // Swap adapter's data
+                        if (mReceipes != null)
+                            mAdapter.swapAdapter(mReceipes);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(jsonRequest);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe, container, false);
+        View rooView = inflater.inflate(R.layout.fragment_recipe, container, false);
+
+        mRecyclerView = (RecyclerView) rooView.findViewById(R.id.rv_recipes);
+        mGridManager = new GridLayoutManager(getActivity(), 2);
+        mAdapter = new RecipesAdapter(getActivity(), null);
+        mRecyclerView.setLayoutManager(mGridManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        return rooView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -84,18 +114,91 @@ public class RecipeFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
+
+class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHolder> {
+    private ArrayList<Recipe> mRecipes;
+    private Context mContext;
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView recipeTitle;
+        final TextView recipeServings;
+        final TextView recipeSteps;
+        final TextView recipeIngredients;
+        final ImageView recipeImage;
+
+        ViewHolder(View v) {
+            super(v);
+            recipeTitle = (TextView) v.findViewById(R.id.tv_recipe_title);
+            recipeServings = (TextView) v.findViewById(R.id.tv_recipe_number_servings);
+            recipeSteps = (TextView) v.findViewById(R.id.tv_recipe_number_steps);
+            recipeIngredients = (TextView) v.findViewById(R.id.tv_recipe_number_ingredients);
+            recipeImage = (ImageView) v.findViewById(R.id.iv_recipe_image);
+        }
+    }
+
+    public RecipesAdapter(Context context, ArrayList<Recipe> recipes) {
+        mContext = context;
+        mRecipes = recipes;
+    }
+
+    public void swapAdapter(ArrayList<Recipe> recipes) {
+        if (recipes == null || recipes.size() == 0) return;
+        mRecipes = recipes;
+        notifyDataSetChanged();
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public RecipesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                        int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_recipe, parent, false);
+
+        final ViewHolder vh = new ViewHolder(v);
+
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(mContext, "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        // Get recipe object at position
+        Recipe recipe = mRecipes.get(position);
+
+        // Set the views
+        holder.recipeTitle.setText(recipe.getName());
+        holder.recipeServings.setText(mContext.getString(
+                R.string.title_servings, recipe.getNumberOfServings()));
+        holder.recipeSteps.setText(mContext.getString(
+                R.string.title_steps, recipe.getSteps().size()));
+        holder.recipeIngredients.setText(mContext.getString(
+                R.string.title_ingredients, recipe.getIngredients().size()));
+
+        // set content description based on recipe's name
+        holder.recipeImage.setContentDescription(mContext.getString(
+                R.string.cd_recipe, recipe.getName()));
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        if (mRecipes == null || mRecipes.size() == 0) return 0;
+        return mRecipes.size();
+    }
+}
+
